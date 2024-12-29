426436fa84900919b0f508e9c55b36f9f0123bec
Stefan Devai <stefandevai@posteo.me>
2024-12-29 12:24:45 +0100

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<style lang="scss">
+  @use 'sass:math';
+
+  h1 {
+    color: blue;
+    box-shadow: 0 0 math.random(10)+px rgba(0, 0, 0, 0.1);
+  }
+</style>
